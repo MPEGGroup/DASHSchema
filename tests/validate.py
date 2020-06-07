0eacd668f07bf7b7cf264bbf7f382500690da5c2
Emmanuel Thomas <emmanuel.thomas@gmx.com>
2020-06-07 09:34:47 +0200

    Update with latest MPD schema, xlink schema has an absolute URL. XLink gets downloaded automatically.

--- tests/validate.py
+++ tests/validate.py
@@ -8,7 +8,6 @@ import unittest
 import logging
 import glob
 from lxml import etree
-from lxml import isoschematron
 
 class TestStringMethods(unittest.TestCase):
     def setUp(self):
@@ -16,17 +15,15 @@ class TestStringMethods(unittest.TestCase):
         logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
         with open('../DASH-MPD.xsd', 'r') as schema_file:
             self.mpd_schema = etree.XMLSchema(etree.parse(schema_file))
-            
-    def test_MPDs(self):
-        i = 0
+
+    def test_mpds(self):
+        """ Test all MPDs found in the repository."""
         for mpd_path in glob.glob('../*.mpd'):
-            i += 1
             self.log.debug('Validating %s', mpd_path)
             with open(mpd_path) as mpd_file:
-                with self.subTest(i=i):
+                with self.subTest(mpd=mpd_path):
                     mpd = etree.parse(mpd_file)
-                    self.assertTrue(self.mpd_schema.validate(mpd))
-
+                    self.mpd_schema.assertValid(mpd)
 
 if __name__ == '__main__':
     unittest.main()
