2b77ab4348ffa313028d91c99abd1bcac504993b
PaulHiggs <paul_higgs@hotmail.com>
2021-05-25 13:34:47 +0100

    clean up test list loading

--- tests/regression-test.py
+++ tests/regression-test.py
@@ -5,15 +5,14 @@ developed by Paul Higgs using Python 3.9.1
 uses the following libraries
  * requests,    python -m pip install requests
  * lxml,        python -m pip install lxml
-
 """
 
 import sys
 import unittest
 import logging
-import glob
 import requests
 import json
+import re
 
 from lxml import etree
 
@@ -23,7 +22,6 @@ MPEGCMAFManifestsFile="MPEG-CMAF-manifests.json"
 DASHIFManifestsFile="DASH-IF-Manifests.json"
 
 DASHIF_dataset_url = "https://raw.githubusercontent.com/Dash-Industry-Forum/Test-Assets-Dataset-Public/master/dataset/data/testvector.json"
-
 
 class TestManifests(unittest.TestCase):
 	def setUp(self):
@@ -50,47 +48,48 @@ class TestManifests(unittest.TestCase):
 					if not self.mpd_schema.validate(mpd):
 						self.fail(self.mpd_schema.error_log.filter_from_errors())
 
-	def loadDatasetURL(self, url):
-		result=[]
-		response=requests.get(url, allow_redirects=True)
-		dataset=json.loads(response.text)
-		for item in dataset:
-			if "url" in item:
-				result.append(item["url"])
-		return result
-
-	def loadDatasetFile(self, filename):
-		result=[]
-		with open(filename, 'r') as manifest_file:
-			dataset=json.load(manifest_file)
-			for item in dataset:
-				if "url" in item:
-					result.append(item["url"])
-		return result
-
 	def check_manifests(self, mpdList, source):
 		for mpdURL in mpdList:
 			self.check_a_manifest(mpdURL, source)
 
+	def isURL(self, value): 
+		return re.search("^https?://.*", value) != None
+
+	def loadDataset(self, fromLocation):
+		result=[]
+		if self.isURL(fromLocation):
+			response=requests.get(fromLocation, allow_redirects=True)
+			dataset=json.loads(response.text)
+		else:
+			with open(fromLocation, 'r') as manifest_file:
+				dataset=json.load(manifest_file)
+		for item in dataset:
+			if "url" in item:
+				if "status" in item:
+					if item["status"].upper() == 'OK':
+						result.append(item["url"])
+				else:
+					result.append(item["url"])
+		return result
 
 	def test_DVB(self):
-		self.check_manifests(self.loadDatasetFile(DVBManifestsFile), "DVB")
+		self.check_manifests(self.loadDataset(DVBManifestsFile), "DVB")
 
 	def test_HbbTV(self):
-		self.check_manifests(self.loadDatasetFile(HbbTVManifestsFile), "HbbTV")
+		self.check_manifests(self.loadDataset(HbbTVManifestsFile), "HbbTV")
 
 	def test_MPEG_CMAF(self):
-		self.check_manifests(self.loadDatasetFile(MPEGCMAFManifestsFile), "MPEG CMAF")
+		self.check_manifests(self.loadDataset(MPEGCMAFManifestsFile), "MPEG CMAF")
 		
 	def test_DASH_IF_list(self):
-		self.check_manifests(self.loadDatasetFile(DASHIFManifestsFile), "DASH-IF Local List")
-				
+		self.check_manifests(self.loadDataset(DASHIFManifestsFile), "DASH-IF Local List")
+			
 	def test_DASH_IF_dataset(self):
-		self.check_manifests(self.loadDatasetURL(DASHIF_dataset_url), "DASH-IF Dataset")
+		self.check_manifests(self.loadDataset(DASHIF_dataset_url), "DASH-IF Dataset")
 	
-#	def test_one(self):
-#		mpd="http://html5.cablelabs.com:8100/cenc/prwv/dash.mpd"
-#		self.check_a_manifest(mpd, "debugging1")
-		
+	def dont_test_one(self):
+		mpd="http://html5.cablelabs.com:8100/cenc/prwv/dash.mpd"
+		self.check_a_manifest(mpd, "debugging1")
+
 if __name__ == '__main__':
     unittest.main()
