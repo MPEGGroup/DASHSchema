331d2f7d02ad0292f8f7b78cde43f33397840d2f
PaulHiggs <paul_higgs@hotmail.com>
2021-06-25 06:08:24 +0100

    check MPP documents agianst DASH-MPD-PATCH.xsd

--- tests/validate.py
+++ tests/validate.py
@@ -13,13 +13,12 @@ class TestDASH(unittest.TestCase):
     def setUp(self):
         self.log = logging.getLogger('TestLog')
         logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-        parser=etree.XMLParser(load_dtd=True, huge_tree=True, resolve_entities=True)
-
+        self.parser=etree.XMLParser(load_dtd=True, huge_tree=True, resolve_entities=True)
 
     def test_mpds(self):
         """ Test all MPDs found in the repository."""
         with open('../DASH-MPD.xsd', 'r') as schema_file:
-            self.mpd_schema = etree.XMLSchema(etree.parse(schema_file, parser))
+            self.mpd_schema = etree.XMLSchema(etree.parse(schema_file, self.parser))
         for mpd_path in glob.glob('../*.mpd'):
             self.log.debug('Validating %s', mpd_path)
             with open(mpd_path) as mpd_file:
@@ -31,7 +30,7 @@ class TestDASH(unittest.TestCase):
     def test_mpps(self):
         """ Test all MPPs found in the repository."""
         with open('../DASH-MPD-PATCH.xsd', 'r') as schema_file:
-            self.mpd_schema = etree.XMLSchema(etree.parse(schema_file, parser))
+            self.mpd_schema = etree.XMLSchema(etree.parse(schema_file, self.parser))
         for mpp_path in glob.glob('../*.mpp'):
             self.log.debug('Validating %s', mpp_path)
             with open(mpp_path) as mpp_file:
